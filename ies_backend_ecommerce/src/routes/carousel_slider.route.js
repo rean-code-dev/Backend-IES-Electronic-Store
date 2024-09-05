7018f02434bf3341dfac71c818c724c577c66895
MOBILE-APP.08 <MOBILE-APP.08@TURBOTECH.COM>
2024-09-05 17:44:00 +0700

    slider API

---
+++ ies_backend_ecommerce/src/routes/carousel_slider.route.js
@@ -0,0 +1,11 @@
+const controller =  require("../controllers/carousel_slider.controller")
+
+const Slider = (app,base_route) =>{
+    app.get(base_route,controller.getAll_Slider)
+    app.get(`${base_route}/:id`,controller.getOne_Slider)
+    app.post(base_route,controller.create_Slider)
+    app.put(base_route,controller.update_Slider)
+    app.delete(`${base_route}/:id`,controller.remove_Slider)
+}
+
+module.exports = Slider
