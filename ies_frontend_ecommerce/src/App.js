89af58d635f41261fc4bbcbb195caf140ac34b90
MOBILE-APP.08 <MOBILE-APP.08@TURBOTECH.COM>
2025-01-30 13:45:52 +0700

    recheck

--- ies_frontend_ecommerce/src/App.js
+++ ies_frontend_ecommerce/src/App.js
@@ -1,52 +1,39 @@
 
-// import {BrowserRouter,Routes,Route} from 'react-router-dom'
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import CategoryPageDash from './dashboard_admin/category_page/CategoryPageDash';
-// import HomePageDash from './dashboard_admin/home_page/HomePageDash';
-// import EmployeePageDash from './dashboard_admin/employee_page/EmployeePageDash';
-// import CustomerPageDash from './dashboard_admin/customer_page/CustomerPageDash';
-// import RouteNoteFound from './dashboard_admin/route_not_found/RouteNotFound';
-// import LoginDashboard from './dashboard_admin/login_page/LoginPageDash';
-// import LayoutDashboardLogin from './components/layout/LayoutDashboardLogin';
-// import LayoutDashboard from './components/layout/LayoutDashboard';
-import WebSocketChat from './connect_socket/channel_socketClient';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import ReceiverPage from './connect_socket/receiverPage';
-import SenderPage from './connect_socket/senderPage';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CategoryPageDash from './dashboard_admin/category_page/CategoryPageDash';
+import HomePageDash from './dashboard_admin/home_page/HomePageDash';
+import EmployeePageDash from './dashboard_admin/employee_page/EmployeePageDash';
+import CustomerPageDash from './dashboard_admin/customer_page/CustomerPageDash';
+import RouteNoteFound from './dashboard_admin/route_not_found/RouteNotFound';
+import LoginDashboard from './dashboard_admin/login_page/LoginPageDash';
+import LayoutDashboardLogin from './components/layout/LayoutDashboardLogin';
+import LayoutDashboard from './components/layout/LayoutDashboard';
+
 function App() {
   return (
-    <Router>
-    <div>
-      <nav>
-        <Link to="/sender">Sender</Link> | <Link to="/receiver">Receiver</Link>
-      </nav>
-      <Routes>
-        <Route path="/sender" element={<SenderPage />} />
-        <Route path="/receiver" element={<ReceiverPage />} />
-      </Routes>
-    </div>
-  </Router>
+   
     
-    // <BrowserRouter> 
-    // <Routes>
-    //   {/* Backend */}
-    //     <Route path="/dashboard" element ={<LayoutDashboard/>}>
-    //     <Route path="" element={<HomePageDash/>} />
-    //       <Route path="category" element={<CategoryPageDash/>} />
-    //       <Route path="employee" element={<EmployeePageDash/>} />
-    //       <Route path="customer" element={<CustomerPageDash/>} />
-    //       <Route path="*" element={<RouteNoteFound/>} />
+    <BrowserRouter> 
+    <Routes>
+      {/* Backend */}
+        <Route path="/dashboard" element ={<LayoutDashboard/>}>
+        <Route path="" element={<HomePageDash/>} />
+          <Route path="category" element={<CategoryPageDash/>} />
+          <Route path="employee" element={<EmployeePageDash/>} />
+          <Route path="customer" element={<CustomerPageDash/>} />
+          <Route path="*" element={<RouteNoteFound/>} />
           
         
-    //     </Route>
-    //   {/* backend login register */}
-    //     <Route path="/dashboard/" element ={<LayoutDashboardLogin/>}>
-    //     <Route path="login" element={<LoginDashboard/>} />
+        </Route>
+      {/* backend login register */}
+        <Route path="/dashboard/" element ={<LayoutDashboardLogin/>}>
+        <Route path="login" element={<LoginDashboard/>} />
       
-    //     </Route>
+        </Route>
       
-    // </Routes>   
-    // </BrowserRouter>
+    </Routes>   
+    </BrowserRouter>
     
   );
 
